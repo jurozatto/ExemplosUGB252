28faa7c6eba24d1e475702b1c365bc02c8ae0b86
jurozatto <juliarozatto6@gmail.com>
2025-08-12 21:05:42 -0300

    Desconto IRPF

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,5 +1,31 @@
 ﻿Console.WriteLine("Informe seu nome: ");
 string nome = Console.ReadLine();
 Console.WriteLine("Informe seu salário: ");
-decimal salarioStr = decimal.Parse(Console.ReadLine());
-Console.WriteLine("Seja bem-vindo(a) " + nome + ". Seu salário é de R$" + salarioStr);
+decimal salario = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Seja bem-vindo(a) " + nome + ". Seu salário é de R$" + salario);
+
+//informe o valor do IRPF a ser pago de acordo com o salário informado. Utilize a tabela real de desconto de IRPF
+if (salario <= 2259.20m)
+{
+    Console.WriteLine("Você não paga taxa.");
+}
+else if (salario >= 2259.20m && salario <= 2826.65m)
+{
+    decimal novoSalario = salario - (salario * 0.075m);
+    Console.WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
+}
+else if (salario >= 2826.66m && salario <= 3751.05m)
+{
+    decimal novoSalario = salario - (salario * 0.15m);
+    Console.WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
+}
+else if (salario >= 3751.06m && salario <= 4664.68m)
+{
+    decimal novoSalario = salario - (salario * 0.225m);
+    Console.WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
+}
+else
+{
+    decimal novoSalario = salario - (salario * 0.275m);
+    Console.WriteLine($"Seu salário com desconto é de {novoSalario:C2}");
+}
