e172512321461a68938e9cf184a99c3ace5fd79c
jurozatto <juliarozatto6@gmail.com>
2025-08-26 20:29:43 -0300

    Calcumo do aumento

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -49,11 +49,74 @@
 
 using ConceitosBasicos.uteis;
 
-SomaDoisNumeros somaDoisNumeros = new SomaDoisNumeros();
-int resultado = somaDoisNumeros.Somar(20, 22);
-Console.WriteLine(resultado);
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escolha um método para executar:");
+        Console.WriteLine("1. Soma de dois números");
+        Console.WriteLine("2. Converter metros em milimetros");
+        Console.WriteLine("3. Calcular o aumento do salário");
+
+        Console.WriteLine("Digite o número da opção: ");
+        int escolha = int.Parse(Console.ReadLine());
+
+        switch (escolha)
+        {
+            case 1:
+                SomaDoisNumeros somaDoisNumeros = new SomaDoisNumeros();
+                Console.WriteLine("Digite o primeiro número: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o segundo número: ");
+                int num2 = int.Parse(Console.ReadLine());
+                int resultado = somaDoisNumeros.Somar(num1, num2);
+                Console.WriteLine($"O resultado da soma é {resultado}");
+                break;
+
+            case 2:
+                MetrosMilimetros metrosMilimetros = new MetrosMilimetros();
+                Console.WriteLine("Digite a quantidade em metros: ");
+                int num3 = int.Parse(Console.ReadLine());
+                int milimetros = MetrosMilimetros.Converter(num3);
+                Console.WriteLine(milimetros); ;
+                break;
+
+            case 3:
+                CalculaAumento calculaAumento = new CalculaAumento();
+                Console.WriteLine("Informe seu salário: ");
+                decimal salario = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Informe seu percentual de aumento: ");
+                float percentual = float.Parse(Console.ReadLine());
+                decimal percentualAumento = calculaAumento.Calcular(salario, percentual);
+                Console.WriteLine($"Para o salário {salario:c2}, o aumento de {percentual}% corresponde a {percentualAumento:c2}");
+                Console.WriteLine($"O novo salário é {salario + percentualAumento:c2}"); ;
+                break;
+
+        }
+    }
+
+}
 
 
-int milimetros = MetrosMilimetros.Converter(2);
-Console.WriteLine(milimetros);
+
+//SomaDoisNumeros somaDoisNumeros = new SomaDoisNumeros();
+//int resultado = somaDoisNumeros.Somar(20, 50);
+//Console.WriteLine(resultado);
+
+
+//int milimetros = MetrosMilimetros.Converter(50);
+//Console.WriteLine(milimetros);
+
+
+//CalculaAumento calculaAumento = new CalculaAumento();
+//Console.WriteLine("Informe seu salário: ");
+//decimal salario = Convert.ToDecimal(Console.ReadLine());
+//Console.WriteLine("Informe seu percentual de aumento: ");
+//float percentual = float.Parse(Console.ReadLine());
+//decimal percentualAumento = calculaAumento.Calcular(salario, percentual);
+//Console.WriteLine($"Para o salário {salario:c2}, o aumento de {percentual}% corresponde a {percentualAumento:c2}");
+//Console.WriteLine($"O novo salário é {salario + percentualAumento:c2}");
+
+
 
