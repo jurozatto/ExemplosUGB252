74c852449420d2878a5dd264c88863c070501573
jurozatto <juliarozatto6@gmail.com>
2025-09-02 20:43:27 -0300

    Teste de Unidade

---
+++ TesteConceitosBasicos/TesteCalculaAumento.cs
@@ -0,0 +1,29 @@
+﻿using ConceitosBasicos.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConceitosBasicos
+{
+    [TestClass]
+    public class TesteCalculaAumento
+    {
+        [TestMethod]
+        public void TestCalcular()
+        {
+            //cenário
+            decimal salario = 1000;
+            float percentual = 20;
+            decimal resultado;
+            CalculaAumento calculaAumento = new CalculaAumento();
+
+            //ação
+            resultado = calculaAumento.Calcular(salario, percentual);
+
+            //verificação
+            Assert.AreEqual(200, resultado);
+        }
+    }
+}
